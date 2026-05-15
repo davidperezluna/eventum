62976c106d6552c7d6c5fea8cc0dd86abe14a04a
dfluna <>
2026-05-15 14:16:27 -0500

    cambios

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
-import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+  withInMemoryScrolling,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
@@ -9,7 +13,11 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(
       routes,
-      withEnabledBlockingInitialNavigation() // Mejora la navegación inicial en producción
+      withEnabledBlockingInitialNavigation(), // Mejora la navegación inicial en producción
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      })
     ),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
